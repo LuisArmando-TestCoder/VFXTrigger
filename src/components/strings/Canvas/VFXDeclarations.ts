960aa32feaa1fc0038cfa45d153113262ecb45cb
Luis Armando Murillo <luisarmando.murillobaltodano@gmail.com>
2022-11-16 18:40:58 -0600

    add warp VFX

--- src/components/strings/Canvas/VFXDeclarations.ts
+++ src/components/strings/Canvas/VFXDeclarations.ts
@@ -2,6 +2,7 @@ import nightSky from "../../../VFX/nightSky"
 import opaque from "../../../VFX/opaque"
 import radar from "../../../VFX/radar"
 import voids from "../../../VFX/voids"
+import warp from "../../../VFX/warp"
 
 type TriggerMap = {
   [index: string]: ((data: { [index: string]: any }) => () => void)[]
@@ -11,6 +12,7 @@ export const functionToTriggers: TriggerMap = {
   q: [radar],
   w: [voids],
   e: [nightSky],
+  r: [warp]
 }
 export const functionToNonAvailableTriggers: TriggerMap = {
   z: [opaque],
