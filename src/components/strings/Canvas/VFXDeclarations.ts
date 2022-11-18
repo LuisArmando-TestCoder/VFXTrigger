43d64b5500d72ba282925fbf1069962cc0b70408
Luis Armando Murillo <luisarmando.murillobaltodano@gmail.com>
2022-11-18 01:09:36 -0600

    set VFX

--- src/components/strings/Canvas/VFXDeclarations.ts
+++ src/components/strings/Canvas/VFXDeclarations.ts
@@ -1,19 +1,25 @@
 import nightSky from "../../../VFX/nightSky"
 import opaque from "../../../VFX/opaque"
 import radar from "../../../VFX/radar"
-import voids from "../../../VFX/voids"
+import boids from "../../../VFX/boids"
 import warp from "../../../VFX/warp"
+import clockLines from "../../../VFX/clockLines"
+import warpBoids from "../../../VFX/warpBoids"
+import trappedBoids from "../../../VFX/trappedBoids"
 
 type TriggerMap = {
   [index: string]: ((data: { [index: string]: any }) => () => void)[]
 }
 
 export const functionToTriggers: TriggerMap = {
-  q: [radar],
-  w: [voids],
-  e: [nightSky],
-  r: [warp]
+  q: [boids],
+  w: [warpBoids],
+  e: [trappedBoids],
+  r: [radar],
+  t: [nightSky],
+  y: [warp],
+  u: [clockLines],
 }
 export const functionToNonAvailableTriggers: TriggerMap = {
-  z: [opaque],
+  m: [opaque],
 }
